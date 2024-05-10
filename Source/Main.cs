fe3153e8a0f7b0ccb0ebd1d2b17bf47196516e6a
Doug Benham <douglasabenham@gmail.com>
2024-05-10 13:15:12 -0700

    Returned charging interruption logic but only if not a militor or tunneler

--- Source/Main.cs
+++ Source/Main.cs
@@ -2,6 +2,7 @@
 using HarmonyLib;
 using Verse;
 using RimWorld;
+using System;
 
 namespace BotsChargeWhenIdle
 {
@@ -33,6 +34,28 @@ namespace BotsChargeWhenIdle
                     __result = JobMaker.MakeJob(JobDefOf.SelfShutdown, result);
                     __result.expiryInterval = 625;
                 }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Building_MechCharger), nameof(Building_MechCharger.Tick), null)]
+    public static class Building_MechCharger_Patch
+    {
+        public static void Postfix(Building_MechCharger __instance, ref Pawn ___currentlyChargingMech)
+        {
+            // every 625 ticks (15 in game minutes), kick the bot off the charger so it will do work if available
+            // if none is available, it will just start charging again
+
+            if (Find.TickManager.TicksGame % 625 == 0 
+                && ___currentlyChargingMech != null
+                // don't interrupt militors who are charging, they will just go back to patrolling which isnt a REAL job
+                && ___currentlyChargingMech.kindDef.defName != "Mech_Militor"
+                // don't interrupt tunnelers who are charging, they move so slowly its counter-productive
+                && ___currentlyChargingMech.kindDef.defName != "Mech_Tunneler"
+                // don't interrupt a bot if it doesnt have at least 10% more than minimum charge OR 90%, whichever is lower (to account for someone choosing to set a minimum charge >90% for some reason).
+                && ___currentlyChargingMech.needs.energy.CurLevelPercentage > Math.Min(.90f, (___currentlyChargingMech.GetMechControlGroup().mechRechargeThresholds.min + .10f)))
+            {
+                ___currentlyChargingMech.jobs.curDriver.ReadyForNextToil();
             }
         }
     }
